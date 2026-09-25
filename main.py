4d7a8cf09a1321ac6bf464ebe11c0044192817c6
web-dev <raghavbhatnagar@gmail.com>
2026-09-25 20:57:01 +0530

    Add FastAPI user endpoint

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app=FastAPI()
+
+class User(BaseModel):
+    name:str
+    age:int
+@app.post("/user")
+def create_user(user:User):
+    return user
